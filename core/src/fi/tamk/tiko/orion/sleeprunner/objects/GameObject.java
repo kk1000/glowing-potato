311bed298056937f72a5f48c96310262305ea60b
steiner3k <joni.korpisalo@gmail.com>
2016-03-02 19:52:03 +0200

    GameObject class changes

--- core/src/fi/tamk/tiko/orion/sleeprunner/objects/GameObject.java
+++ core/src/fi/tamk/tiko/orion/sleeprunner/objects/GameObject.java
@@ -21,12 +21,20 @@ public abstract class GameObject extends Actor {
 
     }
 
+    /**
+     * Constructor for game objects.
+     * @param body used for updating.
+     */
     public GameObject(Body body) {
         this.body = body;
         this.userData = (UserData) body.getUserData();
         screenRectangle = new Rectangle();
     }
 
+    /**
+     * Act method, updates body's rectangle if it's not null (outside of screen).
+     * @param delta = delta timer (1/60)
+     */
     @Override
     public void act(float delta) {
         super.act(delta);
@@ -37,8 +45,14 @@ public abstract class GameObject extends Actor {
         }
     }
 
+    /**
+     * @return user data
+     */
     public abstract UserData getUserData();
 
+    /**
+     * Updates rectangle with transformToScreen method, which multiplies the metric values with WORLD_TO_SCREEN (from Constants) attribute.
+     */
     private void updateRectangle() {
         screenRectangle.x = transformToScreen(body.getPosition().x - userData.getWidth() / 2);
         screenRectangle.y = transformToScreen(body.getPosition().y - userData.getHeight() / 2);
@@ -46,6 +60,11 @@ public abstract class GameObject extends Actor {
         screenRectangle.height = transformToScreen(userData.getHeight());
     }
 
+    /**
+     * Transforms the metric values to screen values with WORLD_TO_SCREEN attribute.
+     * @param n = metric value from updateRectangle-method
+     * @return the multiplied value
+     */
     protected float transformToScreen(float n) {
         return Constants.WORLD_TO_SCREEN * n;
     }
