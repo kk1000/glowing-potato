45fb38fea048b47738bdcbb5e2b4a36c3a714d86
steiner3k <joni.korpisalo@gmail.com>
2016-03-09 14:31:16 +0200

    Background Deep

--- core/src/fi/tamk/tiko/orion/sleeprunner/graphics/BackgroundDeepLayer.java
+++ core/src/fi/tamk/tiko/orion/sleeprunner/graphics/BackgroundDeepLayer.java
@@ -2,19 +2,35 @@ package fi.tamk.tiko.orion.sleeprunner.graphics;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import fi.tamk.tiko.orion.sleeprunner.data.Constants;
 
 /**
  * Game screen's background.
  */
-public class BackgroundDeepLayer extends MovingBackground {
+public class BackgroundDeepLayer extends Actor {
 
     /**
      * Constructor for deep background.
      */
+
+    Texture texture;
+    int srcX;
+
     public BackgroundDeepLayer(){
-        super(new Texture(Gdx.files.internal(Constants.BACKGROUND_IMAGE_PATH)), 10, 2);
+        texture = new Texture(Gdx.files.internal(Constants.BACKGROUND_IMAGE_PATH));
+        texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
     }
 
-}
+    public void draw(Batch batch, float parentAlpha) {
+
+            batch.draw(texture, 0, 0, srcX, 0, Constants.APP_WIDTH, Constants.APP_HEIGHT);
+        }
+    public void act(float delta){
+        super.act(delta);
+        srcX+=1;
+    }
+
+    }
