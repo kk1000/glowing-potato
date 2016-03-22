725699589b50b89663f3550af6d5dd449a4ff345
Eetu <eetu.jarvinen@cs.tamk.fi>
2016-03-22 11:01:37 +0200

    Maximum ground amount and spike pos fix

---
+++ GameObject.java
@@ -0,0 +1,289 @@
+package fi.tamk.tiko.orion.sleeprunner.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+import fi.tamk.tiko.orion.sleeprunner.data.Constants;
+import fi.tamk.tiko.orion.sleeprunner.data.Preference;
+import fi.tamk.tiko.orion.sleeprunner.data.Tile;
+import fi.tamk.tiko.orion.sleeprunner.data.UserData;
+
+/**
+ * Superclass of every game object.
+ * Not designed to construct object from this class.
+ */
+public abstract class GameObject {
+
+    protected World world;
+
+    protected float x;
+    protected float y;
+    protected float density;
+    protected float width;
+    protected float height;
+
+    protected Texture texture;
+
+    protected UserData userData;
+    protected TextureRegion textureRegion;
+    protected Body body;
+    protected float textureWidth;
+    protected float textureHeight;
+
+    protected boolean hasAnimation = false;
+    protected Animation currentAnimation; // THIS NEEDS TO MANUALLY SET ATM.
+    protected TextureRegion currentFrame;
+    protected float stateTime;
+
+    protected Array<Tile> tiles = new Array<Tile>();
+
+    protected Preference prefs;
+
+    /**
+     * Constructor for game objects which got no animation.
+     *
+     * @param world         Box2D World
+     * @param x             X-position.
+     * @param y             Y-position.
+     * @param width         Width of the body.
+     * @param height        Height of the body.
+     * @param density       Body's density.
+     * @param textureRegion The texture.
+     * @param bodyType      Box2D body's type.
+     * @param userData      Box2D body's userdata.
+     */
+    public GameObject(World world, float x, float y, float width, float height, float density, TextureRegion textureRegion, BodyDef.BodyType bodyType, UserData userData) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.density = density;
+        this.textureRegion = textureRegion;
+        this.textureWidth = this.textureRegion.getRegionWidth() / 100f;
+        this.textureHeight = this.textureRegion.getRegionHeight() / 100f;
+        this.width = width;
+        this.height = height;
+        this.userData = userData;
+        createBody(bodyType);
+        createTiles();
+
+        prefs = new Preference();
+    }
+
+    /**
+     * Constructor for animated game objects.
+     *
+     * @param world             Box2D World
+     * @param x                 X-position.
+     * @param y                 Y-position.
+     * @param width             Width of the body.
+     * @param height            Height of the body.
+     * @param density           Body's density.
+     * @param texture           The texture.
+     * @param bodyType          Box2D body's type.
+     * @param userData          Box2D body's userdata.
+     */
+    public GameObject(World world, float x, float y, float width, float height, float density, Texture texture, BodyDef.BodyType bodyType, UserData userData) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.density = density;
+        this.texture = texture;
+        this.textureWidth = this.texture.getWidth() / 100f;
+        this.textureHeight = this.texture.getHeight() / 100f;
+        this.width = width;
+        this.height = height;
+        this.userData = userData;
+        hasAnimation = true;
+
+        // IMPORTANT!
+        // Remember to set animations and current frame!
+        // There should be AnimatedGameObject (TODO)
+        // to properly handle animated game objects and their animations.
+
+        createBody(bodyType);
+
+        prefs = new Preference();
+    }
+
+    /**
+     * Creates Box2D body to the world.
+     */
+    protected void createBody(BodyDef.BodyType bodyType) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = bodyType;
+        bodyDef.position.set(new Vector2( this.x, this.y ) );
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(this.width / 2, this.height / 2);
+
+        Body body = this.world.createBody(bodyDef);
+
+        // Body's creation differs by game objects.
+        if (userData.id.equals("PLAYER")) {
+            // Dynamic body has more specific details.
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.restitution = 0.0f;
+            fixtureDef.friction = 0.0f;
+            fixtureDef.density = this.density;
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        } else if (userData.id.equals("GROUND")) {
+            body.createFixture(shape, this.density);
+        }
+
+        body.resetMassData();
+        body.setUserData(userData);
+
+        shape.dispose();
+        this.body = body;
+    }
+
+    /**
+     * Creates game object's tiles (textures).
+     */
+    public void createTiles() {
+        TextureRegion[] gameObjectsTextureRegions;
+        if (userData.id.equals("GROUND")) {
+            gameObjectsTextureRegions = new TextureRegion[]{GroundObject.LEFT_TEXTURE, GroundObject.MIDDLETOP_TEXTURE, GroundObject.MIDDLE_TEXTURE, GroundObject.RIGHT_TEXTURE};
+        } else if (userData.id.equals("SPIKES")) {
+            gameObjectsTextureRegions = new TextureRegion[]{SpikesObject.LEFT_TEXTURE, SpikesObject.MIDDLE_TEXTURE, SpikesObject.MIDDLE_TEXTURE, SpikesObject.RIGHT_TEXTURE};
+        } else {
+            // Fallback.
+            gameObjectsTextureRegions = new TextureRegion[]{textureRegion, textureRegion, textureRegion, textureRegion};
+        }
+        int pixelHeight = (int) (height * 100f);
+        int pixelWidth = (int) (width * 100f);
+        float tileSize = Constants.WORLD_TO_SCREEN / 100f;
+        float x;
+        float y;
+        for (int i = 0; i < pixelHeight; i += 32) {
+            for ( int j = 0; j < pixelWidth; j += 32 ) {
+                TextureRegion textureRegion;
+                if ( i < pixelHeight - 32 ) {
+                    if (j == 0 && pixelWidth == 32) {
+                        // Body is just one tile length.
+                        textureRegion = gameObjectsTextureRegions[2];
+                    } else if (j == 0 && pixelWidth > 32) {
+                        // Body's first tile and it's larger than one tile length.
+                        textureRegion = gameObjectsTextureRegions[0];
+                    } else if (j == pixelWidth - 32) {
+                        // Body's last tile.
+                        textureRegion = gameObjectsTextureRegions[3];
+                    } else {
+                        // Body's middle tile.
+                        textureRegion = gameObjectsTextureRegions[1];
+                    }
+                } else {
+                    // Not top tile.
+                    if (j == 0 && pixelWidth > 32) {
+                        // Body's first tile and it's larger than one tile length.
+                        textureRegion = gameObjectsTextureRegions[0];
+                    } else if (j == pixelWidth - 32) {
+                        // Body's last tile.
+                        textureRegion = gameObjectsTextureRegions[3];
+                    } else {
+                        // Body's middle tile.
+                        textureRegion = gameObjectsTextureRegions[2];
+                    }
+                }
+                x = (body.getPosition().x - tileSize / 2) - (width / 2 - tileSize / 2) + ( j/100f );
+                y = body.getPosition().y - (i/100f);
+                tiles.add( new Tile( x, y, textureRegion ) );
+            }
+        }
+    }
+
+    /**
+     * Draws game object.
+     *
+     * @param batch Spritebatch.
+     */
+    public void draw(Batch batch) {
+        if (hasAnimation) {
+            stateTime += Gdx.graphics.getDeltaTime();
+            currentFrame = currentAnimation.getKeyFrame(stateTime, true);
+            batch.draw(currentFrame,
+                    body.getPosition().x - currentFrame.getRegionWidth() / 100f / 2,
+                    body.getPosition().y - currentFrame.getRegionHeight() / 100f / 2,
+                    currentFrame.getRegionWidth() / 2 / 100f,
+                    currentFrame.getRegionHeight()/ 2 / 100f,
+                    currentFrame.getRegionWidth() / 100f,
+                    currentFrame.getRegionHeight() / 100f,
+                    1.0f,
+                    1.0f,
+                    body.getTransform().getRotation() * MathUtils.radiansToDegrees);
+        } else {
+            for ( Tile tile : tiles ) {
+                tile.update(Gdx.graphics.getDeltaTime());
+                batch.draw( tile.textureRegion,
+                        tile.x,
+                        tile.y,
+                        Constants.WORLD_TO_SCREEN / 2,
+                        Constants.WORLD_TO_SCREEN / 2,
+                        Constants.WORLD_TO_SCREEN / 100f,
+                        Constants.WORLD_TO_SCREEN / 100f,
+                        1.0f,
+                        1.0f,
+                        0);
+            }
+        }
+    }
+
+    /**
+     * Called in every frame, every game object needs to own one.
+     *
+     * @param delta Delta time.
+     */
+    public abstract void update(float delta);
+
+    /**
+     * Destroys game object.
+     */
+    public void dispose() {
+        userData = null;
+        body.setUserData(null);
+        world.destroyBody(body);
+        Gdx.app.log("GameObject", "Deleted body, there are total of " + world.getBodyCount());
+    }
+
+    /**
+     * Setters.
+     */
+
+    // This is moved in the future.
+    public void setAnimation(Animation animation) {
+        this.currentAnimation = animation;
+    }
+
+    /**
+     * Getters.
+     */
+
+    public UserData getUserData() {
+        return userData;
+    }
+
+    public float getHeight( ) { return height; }
+    public float getWidth() {
+        return width;
+    }
+
+    public float getX( ) { return x; }
+    public float getY( ) { return y; }
+
+    public Body getBody() {
+        return body;
+    }
+
+}
---
+++ MapGenerator.java
@@ -0,0 +1,285 @@
+package fi.tamk.tiko.orion.sleeprunner.utilities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+import fi.tamk.tiko.orion.sleeprunner.data.Constants;
+import fi.tamk.tiko.orion.sleeprunner.objects.GameObject;
+import fi.tamk.tiko.orion.sleeprunner.objects.GroundObject;
+import fi.tamk.tiko.orion.sleeprunner.objects.ShieldPowerUpObject;
+import fi.tamk.tiko.orion.sleeprunner.objects.SpikesObject;
+
+/**
+ * Methods for generating map chunks.
+ */
+public class MapGenerator {
+
+    /**
+     * Is the integer symbol on the given
+     * position in the chunk grid.
+     *
+     * @param grid   The map chunk grid.
+     * @param symbol The integer symbol to look for.
+     * @param x      The x position.
+     * @param y      The y position.
+     */
+    public static boolean isSymbolAtPosition( int[][] grid, int symbol, int x, int y ) {
+        // Check array boundaries.
+        if ( ( x < Constants.CHUNK_MAX_TILES_WIDTH && x > -1 ) &&
+                ( y < Constants.CHUNK_MAX_TILES_HEIGHT && y > -1 ) ) {
+            if ( grid[ y ][ x ] == symbol ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Is there specific symbol vertical row of given amount.
+     *
+     * @param grid   The map chunk grid.
+     * @param symbol The integer symbol to look for.
+     * @param x      The (starting) x position. (grid index)
+     * @param y      The y position. (grid index)
+     * @param amount The vertical row amount.
+     */
+    public static boolean isSymbolInRow(int[][] grid, int symbol, int x, int y, int amount) {
+        for (int i = 0; i < amount; i++) {
+            if (!isSymbolAtPosition(grid, symbol, x - i, y)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Adds ground or empty to the current position.
+     *
+     * @param grid The 2D integer map grid.
+     * @param x    Current x position. (grid index)
+     */
+    private static void generateGroundBlock( int[][] grid, int x ) {
+        int random = MathUtils.random(0, 4); // Probability to get empty block.
+        if (random == 0) {
+            // Try to make empty block.
+            grid[1][x] = Constants.EMPTY_BLOCK;
+            grid[0][x] = Constants.EMPTY_BLOCK;
+            if ( isSymbolInRow( grid, Constants.EMPTY_BLOCK, x, 1, Constants.MAX_EMPTY_AMOUNT ) ||
+                    !isSymbolInRow( grid, Constants.GROUND_BLOCK, x - 1, 1, Constants.MIN_GROUND_AMOUNT ) ) {
+                // There is already maximum amount of empty blocks
+                // or not enough ground according to min value
+                // replace this position with ground.
+                grid[1][x] = Constants.GROUND_BLOCK;
+                grid[0][x] = Constants.GROUND_BLOCK;
+            }
+        } else {
+            // Try to make ground block.
+            grid[1][x] = Constants.GROUND_BLOCK;
+            grid[0][x] = Constants.GROUND_BLOCK;
+            if ( isSymbolInRow( grid, Constants.GROUND_BLOCK, x - 1, 1, Constants.MAX_GROUND_AMOUNT ) ||
+                    !isSymbolInRow( grid, Constants.EMPTY_BLOCK, x - 1, 1, Constants.MIN_EMPTY_AMOUNT ) &&
+                    !isSymbolAtPosition( grid, Constants.GROUND_BLOCK, x - 1, 1 ) ) {
+                // There is not enough of empty blocks according
+                // to the minimum value or
+                // there are already max ground amount,
+                // replace this position with empty.
+                grid[1][x] = Constants.EMPTY_BLOCK;
+                grid[0][x] = Constants.EMPTY_BLOCK;
+            }
+        }
+    }
+
+    /**
+     * Tries to add power up block to the position.
+     *
+     * @param grid The 2D integer map grid.
+     * @param x    Current x position. (grid index)
+     */
+    private static void generatePowerUpBlock( int[][] grid, int x ) {
+        int random = MathUtils.random( 0, 10 ); // Probability to get any power up.
+        if ( random == 0 ) {
+            // Random power ups y position.
+            int y = MathUtils.random( 4, 6 );
+            if ( isSymbolAtPosition( grid, Constants.EMPTY_BLOCK, x, y ) ) {
+                grid[ y ][ x ] = Constants.POWERUP_SHIELD_BLOCK;
+            }
+        }
+    }
+
+    /**
+     * Tries to generate spikes to the position.
+     *
+     * @param grid  The 2D integer map grid.
+     * @param x     Current x position. (grid index)
+     */
+    private static void generateSpikeBlock( int[][] grid, int x ) {
+        int random = MathUtils.random(0, 3); // Probability to get spikes. (Keep in mind empty blocks)
+        if (random == 0) {
+            // Try to make spikes block. There must be empty block.
+            if (isSymbolAtPosition(grid, Constants.EMPTY_BLOCK, x, 0)) {
+                // Fill the empty blocks with spikes.
+                int ii = x;
+                while (isSymbolAtPosition(grid, Constants.EMPTY_BLOCK, ii, 0)) {
+                    grid[0][ii++] = Constants.SPIKES_BLOCK;
+                }
+            }
+        }
+    }
+
+    /**
+     * Generates semi random positions for grounds
+     * and spikes to the 2D integer array.
+     *
+     * @return Filled 2D integer array.
+     */
+    public static int[][] generateMapChunkGrid() {
+        int[][] grid = new int[ Constants.CHUNK_MAX_TILES_HEIGHT ][ Constants.CHUNK_MAX_TILES_WIDTH ];
+        Gdx.app.log( "MapGenerator", "Generating new map chunk grid!" );
+        for ( int i = 0; i < Constants.CHUNK_MAX_TILES_WIDTH; i++ ) {
+            generateGroundBlock(grid, i);
+            generateSpikeBlock(grid, i);
+            //generatePowerUpBlock( grid, i );
+        }
+        return grid;
+    }
+
+    /**
+     * Generates only ground map chunk grid.
+     *
+     * @return Filled 2D integer array.
+     */
+    public static int[][] generateMapChunkIntervalGrid( ) {
+        int[][] grid = new int[ Constants.CHUNK_MAX_TILES_HEIGHT ][ Constants.CHUNK_MAX_TILES_WIDTH ];
+        for ( int i = 0; i < Constants.CHUNK_MAX_TILES_WIDTH; i++ ) {
+            grid[0][i] = Constants.GROUND_BLOCK;
+            grid[1][i] = Constants.GROUND_BLOCK;
+        }
+        return grid;
+    }
+
+    /**
+     * Is the given position in any of the
+     * generated game object's bounds.
+     *
+     * @param x           X-position. (grid index)
+     * @param y           Y-position. (grid index)
+     * @param symbol      Integer symbol for the game object.
+     * @param mapChunk    Current map chunk.
+     */
+    public static boolean isInGameObjectBounds( int x, int y, int symbol, MapChunk mapChunk ) {
+        Array<GameObject> gameObjects = mapChunk.getGameObjects();
+        float offsetLength = ( mapChunk.getOffset() * Constants.CHUNK_WIDTH_PIXELS )/100f;
+        float gameObjectHeight;
+        float gameObjectWidth;
+        float gameObjectX;
+        float gameObjectY;
+        float meterY;
+        float meterX;
+        float scale = 1/100f;
+        for ( GameObject gameObject : gameObjects ) {
+            if ( gameObject.getUserData().symbol == symbol ) {
+                gameObjectWidth = gameObject.getWidth();
+                gameObjectHeight = gameObject.getHeight();
+                gameObjectX = gameObject.getX() - gameObjectWidth/2;
+                gameObjectY = gameObject.getY() - gameObjectHeight/2;
+                meterX = ( x * scale ) + offsetLength;
+                meterY = ( y * scale );
+                if ( ( meterX <= ( gameObjectX + meterX ) && meterX <= ( gameObjectWidth + meterX ) ) &&
+                        ( meterY <= gameObjectY && meterY <= gameObjectHeight ) ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Generates game object with given PIXEL details.
+     *
+     * @param mapChunk  MapChunk where to add this game object.
+     * @param x         X-position.
+     * @param y         Y-position.
+     * @param width     Width.
+     * @param height    Height.
+     * @param symbol    Game object's symbol in the 2D integer grid.
+     * @return          Created GameObject.
+     */
+    public static GameObject generateGameObject( MapChunk mapChunk, int x, int y, int width, int height, int symbol ) {
+        GameObject gameObject = null;
+        World world = mapChunk.getWorld();
+        float offset = mapChunk.getOffset();
+        float scale = 1/100f;
+        float meterHeight = height * scale;
+        float meterWidth = width * scale;
+        float meterX = ( x * scale ) + ( (offset * Constants.CHUNK_WIDTH_PIXELS )/100f );
+        float meterY = y * scale;
+        float centerX = meterWidth/2 + meterX;
+        float centerY = meterHeight/2 + meterY;
+        if ( centerY == 0.16f ) { centerY = 0f; } // Purkka spike y pos fix.
+        if ( symbol == Constants.GROUND_BLOCK ) {
+            gameObject = new GroundObject( world, centerX, centerY, meterWidth, meterHeight );
+        } else if ( symbol == Constants.SPIKES_BLOCK ) {
+            gameObject = new SpikesObject( world, centerX, centerY, meterWidth, meterHeight );
+        } else if ( symbol == Constants.POWERUP_SHIELD_BLOCK ) {
+            gameObject = new ShieldPowerUpObject( world, centerX, centerY, meterWidth, meterHeight );
+        }
+        return gameObject;
+    }
+
+    /**
+     * Generates game objects from the map chunk's 2D integer 'grid' array.
+     */
+    public static void generateGameObjects( MapChunk mapChunk ) {
+        int[][] grid = mapChunk.getGrid();
+        int currentSymbol = 0;
+        int startX = 0;
+        int startY = 0;
+        int rowX = 1;
+        int rowY = 1;
+        // Loop through map's content.
+        for (int i = Constants.CHUNK_MAX_TILES_HEIGHT - 1; i > -1; i--) {
+            for (int j = 0; j < Constants.CHUNK_MAX_TILES_WIDTH; j++) {
+                int currentPos = grid[i][j];
+                if (!isInGameObjectBounds(j, i, currentPos, mapChunk )) {
+                    // 'Combo' ended, make a game object of the gathered details.
+                    if (currentSymbol != currentPos || ( j == Constants.CHUNK_MAX_TILES_WIDTH - 1 ) ) {
+                        if (currentSymbol > 0) {
+                            mapChunk.addGameObject(generateGameObject(mapChunk, startX, startY, rowX * Constants.WORLD_TO_SCREEN, rowY * Constants.WORLD_TO_SCREEN, currentSymbol));
+                        }
+                        // Reset details.
+                        currentSymbol = currentPos;
+                        rowX = 1;
+                        rowY = 1;
+                    }
+                    if (isSymbolAtPosition(grid, currentSymbol, j + 1, i)) {
+                        // If this is row's first, set the game object's starting positions.
+                        if (rowX == 1) {
+                            if (isSymbolAtPosition(grid, currentSymbol, j, i - 1)) {
+                                startY = ((i * Constants.WORLD_TO_SCREEN) - Constants.WORLD_TO_SCREEN);
+                                rowY++;
+                            } else {
+                                startY = i * Constants.WORLD_TO_SCREEN;
+                            }
+                            startX = j * Constants.WORLD_TO_SCREEN;
+                        }
+                        rowX++;
+                    } else {
+                        if (rowX == 1) {
+                            // There is row of one, fix the starting positions.
+                            if (isSymbolAtPosition(grid, currentSymbol, j, i - 1)) {
+                                startY = ((i * Constants.WORLD_TO_SCREEN) - Constants.WORLD_TO_SCREEN);
+                                rowY++;
+                            } else {
+                                startY = i * Constants.WORLD_TO_SCREEN;
+                            }
+                            startX = j * Constants.WORLD_TO_SCREEN;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
