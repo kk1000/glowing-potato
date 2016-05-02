b9642450eb431b8b8c9e90c525f02b1c39e3c76a
steiner3k <joni.korpisalo@gmail.com>
2016-05-02 20:26:17 +0300

    test of item layer

--- core/src/fi/tamk/tiko/orion/sleeprunner/stages/BackgroundStage.java
+++ core/src/fi/tamk/tiko/orion/sleeprunner/stages/BackgroundStage.java
@@ -49,12 +49,12 @@ public class BackgroundStage extends Stage {
     private void setupBackgrounds(){
         deepLayer = new MovingBackground(game, game.resources.assetManager.get("graphics/backgrounds/stars.png",Texture.class), 2.5f, 1);
         addActor(deepLayer);
-        itemLayer = new MovingBackground(game,game.resources.assetManager.get("graphics/backgrounds/items.png", Texture.class),1.5f,3);
-        addActor(itemLayer);
-        midLayer = new MovingBackground(game,game.resources.assetManager.get("graphics/backgrounds/texture_deep_all.png",Texture.class), 1f,2);
+        midLayer = new MovingBackground(game,game.resources.assetManager.get("graphics/backgrounds/texture_deep_all.png",Texture.class), 1.5f,2);
         addActor(midLayer);
-        topLayer = new MovingBackground(game, game.resources.assetManager.get("graphics/backgrounds/clouds.png",Texture.class), 0.5f,4);
-        addActor(topLayer);
+        itemLayer = new MovingBackground(game,game.resources.assetManager.get("graphics/backgrounds/items.png", Texture.class),0.20f,3);
+        addActor(itemLayer);
+       // topLayer = new MovingBackground(game, game.resources.assetManager.get("graphics/backgrounds/clouds.png",Texture.class), 0.5f,4);
+       // addActor(topLayer);
 
     }
 
@@ -62,29 +62,29 @@ public class BackgroundStage extends Stage {
         if(deepLayer.getSpeed()>1) {
             deepLayer.setSpeed(deepLayer.getSpeed() - 0.2f);
         }
-        if(itemLayer.getSpeed()>1f) {
-            itemLayer.setSpeed(itemLayer.getSpeed() - 0.2f);
+        if(itemLayer.getSpeed()>0.1f) {
+            itemLayer.setSpeed(itemLayer.getSpeed() - 0.05f);
         }
         if(midLayer.getSpeed()>0.3f) {
             midLayer.setSpeed(midLayer.getSpeed() - 0.15f);
         }
-        if(topLayer.getSpeed()>0.05f) {
-            topLayer.setSpeed(topLayer.getSpeed() - 0.10f);
-        }
+       // if(topLayer.getSpeed()>0.05f) {
+        //    topLayer.setSpeed(topLayer.getSpeed() - 0.10f);
+       // }
 
     }
     public void resetSpeed(){
         deepLayer.setSpeed(2.5f);
-        itemLayer.setSpeed(1.5f);
-        midLayer.setSpeed(1);
-        topLayer.setSpeed(0.5f);
+        itemLayer.setSpeed(0.20f);
+        midLayer.setSpeed(1.5f);
+       // topLayer.setSpeed(0.5f);
     }
 
     public void setSpikeSpeed(){
         deepLayer.setSpeed(6f);
-        itemLayer.setSpeed(5f);
+        itemLayer.setSpeed(1f);
         midLayer.setSpeed(6f);
-        topLayer.setSpeed(2f);
+      //  topLayer.setSpeed(2f);
     }
 
     public void reset(){
